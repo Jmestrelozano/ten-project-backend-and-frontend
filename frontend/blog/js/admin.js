b8114b404bb49219b46e2428d5d24e023bdef4ad
jmestrelozano <jmestrelozano@gmail.com>
2020-05-04 17:37:53 -0500

    servicios admin listos

--- frontend/blog/js/admin.js
+++ frontend/blog/js/admin.js
@@ -5,34 +5,48 @@ form.addEventListener("submit", enviarDatos);
 lista.addEventListener("click", eliminarDatos)
 
 
+let nuevoArray = [];
 
 function enviarDatos() {
-    /*localstorage*/
+    /*localstorage login*/
     let verLocal = localStorage.getItem("Data")
     let mostrar = JSON.parse(verLocal)
-
     let id = mostrar[0]._id;
 
+
+
     /*envio de datos*/
+    let campoTitulo = document.querySelector("#titulo").value;
     let campoTexto = document.querySelector("#mytextarea").value;
 
+
+    let titulo = document.createElement("h2");
     let div = document.createElement("div")
-    let h2 = document.createElement("h2")
+    let divbotones = document.createElement("div")
+    let divcontenedor = document.createElement("div")
+    let divtitulo = document.createElement("div")
+    let parrafo = document.createElement("p")
     let btnBorrar = document.createElement("button")
     let btnActualizar = document.createElement("button")
-    lista.appendChild(div)
-    div.appendChild(h2)
-    div.appendChild(btnBorrar)
-    div.appendChild(btnActualizar)
-    h2.id = ("titulo")
-    h2.classList = ("act")
-    div.classList = ("content")
-    btnBorrar.id = ("borrar-dato")
-    btnBorrar.classList = ("fas fa-times icono")
-    btnActualizar.classList = ("fas fa-redo-alt icono")
-    h2.innerHTML += campoTexto
+    lista.appendChild(divcontenedor)
+    divcontenedor.classList = ("contenedor-lista")
+    divcontenedor.id = ("contenedor-id")
 
-    let titulo = document.querySelector("#titulo").textContent;
+    divcontenedor.appendChild(div)
+    div.appendChild(divtitulo)
+    div.appendChild(divbotones)
+    divtitulo.appendChild(titulo)
+    divtitulo.appendChild(parrafo)
+    divbotones.appendChild(btnBorrar)
+    divbotones.appendChild(btnActualizar)
+    parrafo.classList = ("act")
+    div.classList = ("content")
+    btnBorrar.classList = ("fas fa-times icono borrar")
+    btnActualizar.classList = ("fas fa-redo-alt icono")
+    btnBorrar.id = ("borrar-dato")
+    parrafo.id = ("contenido")
+    titulo.innerHTML += campoTitulo
+    parrafo.innerHTML += campoTexto
 
 
     let url = "https://bloggea.herokuapp.com/post/createPost/"
@@ -42,30 +56,77 @@ function enviarDatos() {
                 "Content-Type": "application/json;charset=utf-8",
             },
             body: JSON.stringify({
-                title: titulo,
+                title: campoTitulo,
                 content: campoTexto
             })
         })
         .then(res => res.json())
         .then(function ver(info) {
             console.log("sucess", info)
+            guardarArray(info)
 
         })
         .catch(error => console.error("error", error))
+    listarPost();
+}
+
+
+function guardarArray(info) {
+    nuevoArray.push(info.data)
+    let local = localStorage.setItem("DataPost", JSON.stringify(nuevoArray))
+}
+
+
+function eliminarDatos(e) {
+    let blog, blogid;
+    /*localstorage login*/
+    let verLocal = localStorage.getItem("Data")
+    let mostrar = JSON.parse(verLocal)
+
+    let id = mostrar[0]._id;
+
+    /* localstorage Crearpost*/
+    let localPost = localStorage.getItem("DataPost")
+    let mostrarPost = JSON.parse(localPost)
+    let idpost = mostrarPost[0]._id;
+
+
+    console.log(id, idpost)
+    let url = "https://bloggea.herokuapp.com/post/deletePost/"
+    fetch(url + id + '/' + idpost, {
+            method: 'delete'
+        })
+        .then(response => response.json())
+        .then(respuesta => console.log("respuesta", respuesta))
+        .catch(error => console.error("error", error))
+
+
+    if (e.target.classList.contains("borrar")) {
+        e.target.parentElement.parentElement.remove();
+
+        blog = e.target.parentElement.parentElement;
+        blogid = curso.querySelector("button").getAttribute("idpost")
+
+    }
 
 }
 
-function eliminarDatos() {
-    let btnBorrar = document.querySelector("#borrar-dato")
+function listarPost() {
 
-    let brn = btnBorrar.parentElement.parentElement.children;
-    console.log(brn)
-    for (let i = 1; i <= brn.length; i++) {
-        let revisa = brn[i];
-        revisa.remove();
+    /*localstorage login*/
+    let verLocal = localStorage.getItem("Data")
+    let mostrar = JSON.parse(verLocal)
+    let id = mostrar[0]._id;
 
-
-    }
+    let url = "https://bloggea.herokuapp.com/user/userPost/"
+    fetch(url + id, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json())
+        .then(respues => console.log("sucess", respues))
+        .catch(error => console.error("error", error))
 }
 
 tinymce.init({
